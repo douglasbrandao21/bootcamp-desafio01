4f17e8549fb2ca9759f419274fcfba6c9fdaf891
Douglas Brandão <38235074+douglasbrandao21@users.noreply.github.com>
2019-12-05 19:18:17 -0300

    Loader from User model has created

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,17 @@
 import { Router } from 'express';
 
+import User from './app/models/User';
+
 const routes = new Router();
 
-routes.get('/', (request, response) =>
-  response.json({ message: 'Hello World' })
-);
+routes.get('/', async (request, response) => {
+  const user = await User.create({
+    name: 'Douglas Brandão',
+    email: 'douglasbrando5@gmail.com',
+    password_hash: '123',
+  });
+
+  response.json(user);
+});
 
 export default routes;
