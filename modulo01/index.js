71c9f9d9b68af80c36c1a69151a3e34972bcf8e1
douglasbrandao21 <douglasbrando5@gmail.com>
2020-03-17 07:13:09 -0300

    CRUD Finished

--- modulo01/index.js
+++ modulo01/index.js
@@ -1,30 +1,69 @@
 const express = require("express");
 
 const server = express();
+server.use(express.json());
+
+const users = [
+  {
+    id: "0",
+    name: "Douglas Brandão"
+  },
+  {
+    id: "1",
+    name: "Douglas Canevarollo"
+  },
+  {
+    id: "2",
+    name: "Douglas Honda"
+  }
+];
 
 // Query params = /users/?name="Douglas Brandão"
 // Route params = /users/1
 // Request body = body das requisições HTTP (POST e PUT)
 
-const users = [
-  {
-    id: "1",
-    name: "Douglas Brandão"
-  },
-  {
-    id: "2",
-    name: "Douglas Canevarollo"
-  },
-  {
-    id: "3",
-    name: "Douglas Honda"
-  }
-];
+// Index
+server.get("/users", (request, response) => {
+  return response.json(users);
+});
 
+// Show
 server.get("/users/:id", (request, response) => {
   const { id } = request.params;
 
-  response.json(users[id]);
+  return response.json(users[id]);
+});
+
+// Store
+server.post("/users", (request, response) => {
+  const { name } = request.body;
+
+  users.push({
+    id: users.length + 1,
+    name
+  });
+
+  return response.json(users);
+});
+
+// Update
+server.put("/users/:id", (request, response) => {
+  const { id } = request.params;
+
+  const { name } = request.body;
+
+  users[id].name = name;
+
+  return response.json(users[id]);
+});
+
+// Destroy
+server.delete("/users/:id", (request, response) => {
+  const { id } = request.params;
+
+  users.splice(id, 1);
+
+  return response.json({ message: "User deleted" });
 });
 
 server.listen(3000);
